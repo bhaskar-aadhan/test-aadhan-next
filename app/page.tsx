c9bdf4638d2a3d0ab28c8cdbb45b6c1adbce316f
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2025-04-21 10:58:40 +0530

    third-commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,22 @@ const Home = () => {
       <video autoPlay muted loop>
         <source src="https://static.indusapps.in/assets/videos/hero-video.mp4" type="video/mp4" />
       </video>
+      <br />
+      <video autoPlay muted loop>
+        <source src="https://static.indusapps.in/assets/videos/aboutus-video.mp4" type="video/mp4" />
+      </video>
+      <video autoPlay muted loop>
+        <source src="https://static.indusapps.in/assets/videos/about-aadhan.mp4" type="video/mp4" />
+      </video>
+      <video autoPlay muted loop>
+        <source src="https://static.indusapps.in/assets/videos/channel-videos/hi/insta_aadhan_hindi.mp4" type="video/mp4" />
+      </video>
+      <video autoPlay muted loop>
+        <source src="https://static.indusapps.in/assets/videos/channel-videos/ta/insta_aadhan_aanmeegam.mp4" type="video/mp4" />
+      </video>
+      <video autoPlay muted loop>
+        <source src="https://static.indusapps.in/assets/videos/channel-videos/te/insta_aadhan_health.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 };
